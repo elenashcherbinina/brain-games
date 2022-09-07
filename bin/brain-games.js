7b986b4aeb2119fd51aa843cf6fe9611f614ef9e
Elena Shcherbinina <el.shcherbinina@outlook.com>
2022-09-07 23:20:05 +0300

    fixed errors

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
+import userName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 
-import userName from '../src/cli.js';
 userName();
-
-
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export default (userName) => { 
-const name = readlineSync.question('May I have your name?  ');
-console.log('Hello, ' + name + '!');
+export default (userName) => {
+  const name = readlineSync.question('May I have your name?  ');
+  console.log(`Hello, ${name}!`);
+  return userName;
 };
-
