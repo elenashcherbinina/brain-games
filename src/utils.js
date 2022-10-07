de4512fbc08b1a4043ef854caa61c60b23a0b11f
Elena Shcherbinina <el.shcherbinina@outlook.com>
2022-10-07 16:54:37 +0300

    new functions randomIndex and getProgression

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,15 @@
-const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-export default getRandomNumber;
+export const getRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
+
+export const getProgression = (start, step) => {
+  const progression = [];
+  let countElement = 0;
+  const maxElements = 10;
+
+  for (let number = start; countElement < maxElements; number += step) {
+    progression.push(number);
+    countElement += 1;
+  }
+  return progression;
+};
