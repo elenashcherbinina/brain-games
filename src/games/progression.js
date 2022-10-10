85168babed28eefe9f7e1ad5d75d0829abb3b901
Elena Shcherbinina <el.shcherbinina@outlook.com>
2022-10-10 15:04:15 +0300

    updatedfunction getProgression

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,18 +1,28 @@
 import run from '../index.js';
-import { getRandomNumber, getProgression } from '../utils.js';
+import { getRandomNumber } from '../utils.js';
 
 const description = 'What number is missing in the progression?';
 const minRange = 1;
 const maxRange = 100;
 const minStep = 1;
 const maxStep = 5;
-const minLength = 1;
+const minLength = 0;
 const maxLength = 9;
+
+const getProgression = (start, step, progressionLength) => {
+  const progression = [];
+  for (let i = 1; i <= progressionLength; i += 1) {
+    const number = start + step * i;
+    progression.push(number);
+  }
+  return progression;
+};
 
 const getTask = () => {
   const start = getRandomNumber(minRange, maxRange);
   const step = getRandomNumber(minStep, maxStep);
-  const progression = getProgression(start, step);
+  const progressionLength = 10;
+  const progression = getProgression(start, step, progressionLength);
   const missed = getRandomNumber(minLength, maxLength);
   const correctAnswer = String(progression[missed]);
   progression[missed] = '..';
@@ -21,4 +31,6 @@ const getTask = () => {
   return [question, correctAnswer];
 };
 
-export default () => run(description, getTask);
+export default () => {
+  run(description, getTask);
+};
