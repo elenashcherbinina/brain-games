8b37bd625775d56c450b5f89b24d64631af2bb88
Elena Shcherbinina <el.shcherbinina@outlook.com>
2022-10-10 15:01:21 +0300

    removed function getProgression

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,3 @@
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-export const getRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
-
-export const getProgression = (start, step) => {
-  const progression = [];
-  let countElement = 0;
-  const maxElements = 10;
-
-  for (let number = start; countElement < maxElements; number += step) {
-    progression.push(number);
-    countElement += 1;
-  }
-  return progression;
-};
+export const getRandomIndex = (data) => Math.floor(Math.random() * data.length);
